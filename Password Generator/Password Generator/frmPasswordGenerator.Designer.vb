b86517b445f913c1817282e5488fc6873eb42e5e
Mark LeBlanc <37788275+MarkLeBlanc256@users.noreply.github.com>
2019-10-20 13:55:50 -0500

    Add files via upload

---
+++ Password Generator/Password Generator/frmPasswordGenerator.Designer.vb
@@ -0,0 +1,172 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmPasswordGenerator
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.txtNumCharacters = New System.Windows.Forms.TextBox()
+        Me.btnMinus1 = New System.Windows.Forms.Button()
+        Me.btnPlus1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtOutput = New System.Windows.Forms.TextBox()
+        Me.btnCreate = New System.Windows.Forms.Button()
+        Me.chkSpecialChar = New System.Windows.Forms.CheckBox()
+        Me.chkUppercase = New System.Windows.Forms.CheckBox()
+        Me.chkLowercase = New System.Windows.Forms.CheckBox()
+        Me.chkNumbers = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'txtNumCharacters
+        '
+        Me.txtNumCharacters.Location = New System.Drawing.Point(115, 36)
+        Me.txtNumCharacters.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.txtNumCharacters.Name = "txtNumCharacters"
+        Me.txtNumCharacters.Size = New System.Drawing.Size(30, 20)
+        Me.txtNumCharacters.TabIndex = 0
+        Me.txtNumCharacters.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'btnMinus1
+        '
+        Me.btnMinus1.Location = New System.Drawing.Point(89, 36)
+        Me.btnMinus1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnMinus1.Name = "btnMinus1"
+        Me.btnMinus1.Size = New System.Drawing.Size(21, 19)
+        Me.btnMinus1.TabIndex = 1
+        Me.btnMinus1.Text = "-"
+        Me.btnMinus1.UseVisualStyleBackColor = True
+        '
+        'btnPlus1
+        '
+        Me.btnPlus1.Location = New System.Drawing.Point(148, 36)
+        Me.btnPlus1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnPlus1.Name = "btnPlus1"
+        Me.btnPlus1.Size = New System.Drawing.Size(21, 19)
+        Me.btnPlus1.TabIndex = 2
+        Me.btnPlus1.Text = "+"
+        Me.btnPlus1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 38)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(43, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Length:"
+        '
+        'txtOutput
+        '
+        Me.txtOutput.Font = New System.Drawing.Font("Lucida Sans", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtOutput.Location = New System.Drawing.Point(9, 229)
+        Me.txtOutput.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.txtOutput.Name = "txtOutput"
+        Me.txtOutput.Size = New System.Drawing.Size(164, 20)
+        Me.txtOutput.TabIndex = 4
+        '
+        'btnCreate
+        '
+        Me.btnCreate.Location = New System.Drawing.Point(9, 197)
+        Me.btnCreate.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnCreate.Name = "btnCreate"
+        Me.btnCreate.Size = New System.Drawing.Size(163, 27)
+        Me.btnCreate.TabIndex = 5
+        Me.btnCreate.Text = "Create Password"
+        Me.btnCreate.UseVisualStyleBackColor = True
+        '
+        'chkSpecialChar
+        '
+        Me.chkSpecialChar.AutoSize = True
+        Me.chkSpecialChar.Location = New System.Drawing.Point(22, 168)
+        Me.chkSpecialChar.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.chkSpecialChar.Name = "chkSpecialChar"
+        Me.chkSpecialChar.Size = New System.Drawing.Size(137, 17)
+        Me.chkSpecialChar.TabIndex = 6
+        Me.chkSpecialChar.Text = "Use Special Characters"
+        Me.chkSpecialChar.UseVisualStyleBackColor = True
+        '
+        'chkUppercase
+        '
+        Me.chkUppercase.AutoSize = True
+        Me.chkUppercase.Location = New System.Drawing.Point(22, 102)
+        Me.chkUppercase.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.chkUppercase.Name = "chkUppercase"
+        Me.chkUppercase.Size = New System.Drawing.Size(135, 17)
+        Me.chkUppercase.TabIndex = 7
+        Me.chkUppercase.Text = "Use Uppercase Letters"
+        Me.chkUppercase.UseVisualStyleBackColor = True
+        '
+        'chkLowercase
+        '
+        Me.chkLowercase.AutoSize = True
+        Me.chkLowercase.Location = New System.Drawing.Point(22, 124)
+        Me.chkLowercase.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.chkLowercase.Name = "chkLowercase"
+        Me.chkLowercase.Size = New System.Drawing.Size(135, 17)
+        Me.chkLowercase.TabIndex = 8
+        Me.chkLowercase.Text = "Use Lowercase Letters"
+        Me.chkLowercase.UseVisualStyleBackColor = True
+        '
+        'chkNumbers
+        '
+        Me.chkNumbers.AutoSize = True
+        Me.chkNumbers.Location = New System.Drawing.Point(22, 146)
+        Me.chkNumbers.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.chkNumbers.Name = "chkNumbers"
+        Me.chkNumbers.Size = New System.Drawing.Size(90, 17)
+        Me.chkNumbers.TabIndex = 9
+        Me.chkNumbers.Text = "Use Numbers"
+        Me.chkNumbers.UseVisualStyleBackColor = True
+        '
+        'frmPasswordGenerator
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(197, 258)
+        Me.Controls.Add(Me.chkNumbers)
+        Me.Controls.Add(Me.chkLowercase)
+        Me.Controls.Add(Me.chkUppercase)
+        Me.Controls.Add(Me.chkSpecialChar)
+        Me.Controls.Add(Me.btnCreate)
+        Me.Controls.Add(Me.txtOutput)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnPlus1)
+        Me.Controls.Add(Me.btnMinus1)
+        Me.Controls.Add(Me.txtNumCharacters)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Name = "frmPasswordGenerator"
+        Me.Text = "Password Generator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtNumCharacters As TextBox
+    Friend WithEvents btnMinus1 As Button
+    Friend WithEvents btnPlus1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtOutput As TextBox
+    Friend WithEvents btnCreate As Button
+    Friend WithEvents chkSpecialChar As CheckBox
+    Friend WithEvents chkUppercase As CheckBox
+    Friend WithEvents chkLowercase As CheckBox
+    Friend WithEvents chkNumbers As CheckBox
+End Class
---
+++ Password Generator/Password Generator/frmPasswordGenerator.vb
@@ -0,0 +1,132 @@
+﻿Public Class frmPasswordGenerator
+
+    Private Sub frmPasswordGenerator_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtNumCharacters.Text = "8"
+    End Sub
+
+
+    Private Sub btnCreate_Click(sender As Object, e As EventArgs) Handles btnCreate.Click
+
+        txtOutput.Text = String.Empty
+
+        'Makes sure length text box has a numeric value. If not, inform the user
+        If IsNumeric(txtNumCharacters.Text) Then
+#Region "Old Code"
+            'Dim length = CInt(txtNumCharacters.Text)   'Length of password
+            'Dim charType As Integer = 0    'Stores value for type of each character
+            'Dim password As String = String.Empty  'Used to store the password
+            'Dim randNum As New Random    'Used for randomly selecting each character
+            'Dim typeSelecter As New Random   'Generated to determine type of each character
+
+            'Dim useSpecial As Integer
+            ''If the special characters check box is checked, then the upper range of typeSelector will be set to
+            ''4, allowing special characters to be chosen. If not, then it will be 3, preventing them from being chosen.
+            'If chkSpecialChar.Checked = True Then
+            '    useSpecial = 4
+            'Else
+            '    useSpecial = 3
+            'End If
+
+            'For i = 1 To length    'Generates the password
+            '    charType = typeSelecter.Next(0, useSpecial)
+            '    '
+            '    If charType = 0 Then   'Uppercase letter
+            '        password &= Chr(randNum.Next(65, 91))
+            '    ElseIf charType = 1 Then   'Lowercase letter
+            '        password &= Chr(randNum.Next(97, 123))
+            '    ElseIf charType = 2 Then   'Number
+            '        password &= randNum.Next(0, 9).ToString
+            '    ElseIf charType = 3 Then   'Special Character
+            '        password &= Chr(randNum.Next(33, 46))
+            '    End If
+            'Next
+
+            'txtOutput.Text = password   'Outputs the password
+#End Region
+            'Check to see if any of the character select check boxes are checked. If not, inform the user.
+            If chkUppercase.Checked = True Or chkLowercase.Checked = True Or chkNumbers.Checked = True Or chkSpecialChar.Checked = True Then
+                Dim length As Integer = CInt(txtNumCharacters.Text)   'Number of characters in the password
+                Dim selectedChars As New ArrayList   'Stores ASCII values of selected characters - Used when generating password
+                Dim password As String = String.Empty   'Stores password
+                Dim randNum As New Random   'Random number used for generating password
+
+                'Adds ASCII values for uppercase letters to selectable characters
+                If chkUppercase.Checked = True Then
+                    For i = 65 To 90
+                        selectedChars.Add(i)
+                    Next
+                End If
+                'Adds ASCII values for lowercase letters
+                If chkLowercase.Checked = True Then
+                    For i = 97 To 122
+                        selectedChars.Add(i)
+                    Next
+                End If
+                'Adds ASCII values for numbers
+                If chkNumbers.Checked = True Then
+                    For i = 48 To 57
+                        selectedChars.Add(i)
+                    Next
+                End If
+                'Adds ASCII values for special characters
+                'Plan to add feature to select specific special characters user wants
+                If chkSpecialChar.Checked = True Then
+                    For i = 33 To 47
+                        selectedChars.Add(i)
+                    Next
+                    For i = 58 To 64
+                        selectedChars.Add(i)
+                    Next
+                    For i = 91 To 96
+                        selectedChars.Add(i)
+                    Next
+                    For i = 123 To 126
+                        selectedChars.Add(i)
+                    Next
+                End If
+
+
+                'Generates password. randNum is used to select an ASCII value from selectedChars, which is converted into
+                'a character and added to the password
+                For i = 1 To length
+                    password &= Chr(selectedChars(randNum.Next(0, selectedChars.Count)))
+                Next
+                'Outputs password
+                txtOutput.Text = password
+            Else
+                MessageBox.Show("Please select at least one character type.", "Error")
+            End If
+        Else
+            MessageBox.Show("Length must be numeric.", "Error")
+            txtNumCharacters.Focus()
+        End If
+    End Sub
+
+    Private Sub btnMinus1_Click(sender As Object, e As EventArgs) Handles btnMinus1.Click
+        'Decreases value in txtNumCharacters by 1
+        If IsNumeric(txtNumCharacters.Text) Then
+            If txtNumCharacters.Text > 0 Then
+                txtNumCharacters.Text = (CInt(txtNumCharacters.Text) - 1).ToString
+            End If
+        End If
+    End Sub
+
+    Private Sub btnPlus1_Click(sender As Object, e As EventArgs) Handles btnPlus1.Click
+        'Increases value in txtNumCharacters by 1
+        If IsNumeric(txtNumCharacters.Text) Then
+            txtNumCharacters.Text = (CInt(txtNumCharacters.Text) + 1).ToString
+        End If
+    End Sub
+
+    Private Sub txtNumCharacters_TextChanged(sender As Object, e As EventArgs) Handles txtNumCharacters.TextChanged
+        If IsNumeric(txtNumCharacters.Text) Then
+            If CInt(txtNumCharacters.Text) > 5000 Then
+                txtNumCharacters.Text = 5000
+                MessageBox.Show("5000 is the maximum length allowed.", "Notification")
+            ElseIf CInt(txtNumCharacters.Text) <= 0 Then
+                txtNumCharacters.Text = 8
+                MessageBox.Show("Password cannot be zero or less characters.", "Notification")
+            End If
+        End If
+    End Sub
+End Class
